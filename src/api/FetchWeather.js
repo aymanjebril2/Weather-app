b8a9b7ef0128810721c941528592835786cae4d1
Ayman Omer <aymangebrilomer@gmail.com>
2021-01-03 06:02:27 -0500

    new update

--- src/api/FetchWeather.js
+++ src/api/FetchWeather.js
@@ -2,12 +2,9 @@ import axios from "axios";
 import dotenv from "dotenv";
 
 dotenv.config();
-// const BASE_URL = process.env.BASE_URL;
-// const API_KEY = process.env.API_KEY;
-console.log(process.env.REACT_APP_API_KEY);
 
-const BASE_URL = "https://api.openweathermap.org/data/2.5/weather";
-const API_KEY = "ace71986cb28addd3a6e2aafd7084f41";
+const BASE_URL = process.env.REACT_APP_BASE_URL;
+const API_KEY = process.env.REACT_APP_API_KEY;
 
 async function FetchWeather(query) {
   const { data } = await axios.get(BASE_URL, {
