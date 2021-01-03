46c7a62141665e93bf358031878080eac288f5f5
Ayman Omer <aymangebrilomer@gmail.com>
2021-01-03 07:33:35 -0500

    updat my env

--- src/api/FetchWeather.js
+++ src/api/FetchWeather.js
@@ -2,6 +2,7 @@ import axios from "axios";
 import dotenv from "dotenv";
 
 dotenv.config();
+console.log(process.env.REACT_APP_BASE_URL);
 
 async function FetchWeather(query) {
   const { data } = await axios.get(process.env.REACT_APP_BASE_URL, {
