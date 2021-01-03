485b3f5992493408831cf9df910c7eb755371ccd
Ayman Omer <aymangebrilomer@gmail.com>
2021-01-03 07:19:59 -0500

    new update

--- src/api/FetchWeather.js
+++ src/api/FetchWeather.js
@@ -3,15 +3,12 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const BASE_URL = process.env.REACT_APP_BASE_URL;
-const API_KEY = process.env.REACT_APP_API_KEY;
-
 async function FetchWeather(query) {
-  const { data } = await axios.get(BASE_URL, {
+  const { data } = await axios.get(process.env.REACT_APP_BASE_URL, {
     params: {
       q: query,
       units: "metric",
-      APPID: API_KEY,
+      APPID: process.env.REACT_APP_API_KEY,
     },
   });
   return data;
