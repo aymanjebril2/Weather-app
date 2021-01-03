893a53c16c863abba3bb76e86f53c18497a830c8
Ayman Omer <aymangebrilomer@gmail.com>
2021-01-02 20:50:21 -0500

    updat

--- src/api/FetchWeather.js
+++ src/api/FetchWeather.js
@@ -2,11 +2,11 @@ import axios from "axios";
 import dotenv from "dotenv";
 
 dotenv.config();
-const BASE_URL = process.env.BASE_URL;
-const API_KEY = process.env.API_KEY;
+// const BASE_URL = process.env.BASE_URL;
+// const API_KEY = process.env.API_KEY;
 
-// const BASE_URL = "https://api.openweathermap.org/data/2.5/weather";
-// const API_KEY = "ace71986cb28addd3a6e2aafd7084f41";
+const BASE_URL = "https://api.openweathermap.org/data/2.5/weather";
+const API_KEY = "ace71986cb28addd3a6e2aafd7084f41";
 
 async function FetchWeather(query) {
   const { data } = await axios.get(BASE_URL, {
